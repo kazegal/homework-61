9c78c3080a778a5fd6fc35d58cb4820b62281b30
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-06-20 16:59:13 +0600

    Создал компонент Country который получает данные о странах через API, отображает их в виде списка и предоставляет возможность выбора страны с помощью функции обратного вызова countrySelect

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import AboutCountries from "./container/AboutCountries";
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <AboutCountries />
+        </>
+    );
+}
+
+export default App;
